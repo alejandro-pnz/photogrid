b267f9f15b196fe8ba142b2f4c3b7a5dd364a014
Александр Кожевников <pnz.alexander.kozhevnikov@gmail.com>
2015-11-21 11:57:51 +0300

    images OutOfMemoryError has fixed

--- app/src/main/java/akozhevnikov/photogrid/ImageRecyclerViewAdapter.java
+++ app/src/main/java/akozhevnikov/photogrid/ImageRecyclerViewAdapter.java
@@ -1,12 +1,15 @@
 package akozhevnikov.photogrid;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.List;
 
@@ -16,6 +19,7 @@ public class ImageRecyclerViewAdapter extends RecyclerView.Adapter<ImageViewHold
 
     private List<ImageItem> itemList;
     private Context context;
+    private int parentWidth = 0;
 
     public ImageRecyclerViewAdapter(Context context, List<ImageItem> itemList) {
         this.context = context;
@@ -27,16 +31,40 @@ public class ImageRecyclerViewAdapter extends RecyclerView.Adapter<ImageViewHold
         View layoutView =
                 LayoutInflater.from(parent.getContext()).inflate(R.layout.gallery_card_item, null);
         ImageViewHolder imageViewHolder = new ImageViewHolder(layoutView);
+        parentWidth = parent.getWidth();
         return imageViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(ImageViewHolder holder, int position) {
+    public void onBindViewHolder(final ImageViewHolder holder, int position) {
         Picasso.with(context)
                 .load(itemList.get(position).getLink())
                 .error(R.drawable.error_image)
                 .placeholder(R.drawable.progress_animation)
-                .into(holder.image);
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        int targetWidth = parentWidth / 2;
+                        float ratio = (float) bitmap.getHeight() / (float) bitmap.getWidth();
+                        float heightFloat = ((float) targetWidth) * ratio;
+
+                        final android.view.ViewGroup.MarginLayoutParams layoutParams
+                                = (ViewGroup.MarginLayoutParams) holder.image.getLayoutParams();
+
+                        layoutParams.height = (int) heightFloat;
+                        layoutParams.width = (int) targetWidth;
+                        holder.image.setLayoutParams(layoutParams);
+                        holder.image.setImageBitmap(bitmap);
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+                    }
+                });
     }
 
     @Override
